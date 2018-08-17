30e0bc637832303c74c597086f1aa752fe6c2fe4
Masafumi Horimoto on dv6 <horimoto@holly-linux.com>
2018-08-17 16:58:46 +0900

    SHT21のライブラリ化

--- measure_env.py
+++ measure_env.py
@@ -4,38 +4,44 @@ import subprocess
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
 import time
+import sht21  # SHT2xのライブラリをimportする。試験
 
 # 現在時刻の取得
 now = time.strftime('%Y-%m-%d %H:%M:%S')
 #----------------------
 #SHT-25による温湿度計測
 #----------------------
-ec = 5
-while True:
-  o = subprocess.Popen("/usr/local/lib/python2.7/site-packages/sht21.py",shell=True,stdout=subprocess.PIPE)
-  line = o.stdout.readline()
-  if (line==""):
-    ec = ec - 1
-    if (ec>0):
-      continue
-    else:
-      print >> sys.stderr, "{0} SHT2x READ Error.".format(now)
-  else:
-    ec = 5
-    try:
-      temp = float( line.split()[1] )
-    except:
-      time.sleep(0.3)
-      continue
-    temp = int(temp * 100)
-    line = o.stdout.readline()
-    try:
-      humi = float( line.split()[1] )
-    except:
-      time.sleep(0.3)
-      continue
-    humi = int(humi * 100)
-  break
+with sht21.SHT21(0) as sht21:
+  temp = float(sht21.read_temperature())
+  humi = float(sht21.read_humidity())
+
+#
+#ec = 5   # Error Count : I2C通信などでエラーになった場合、リトライする回数を指定する。
+#while True:
+#  o = subprocess.Popen("/usr/local/lib/python2.7/site-packages/sht21.py",shell=True,stdout=subprocess.PIPE)
+#  line = o.stdout.readline()
+#  if (line==""):
+#    ec = ec - 1
+#    if (ec>0):
+#      continue
+#    else:
+#      print >> sys.stderr, "{0} SHT2x READ Error.".format(now)
+#  else:
+#    ec = 5   # Error Count : I2C通信などでエラーになった場合、リトライする回数を指定する。
+#    try:
+#      temp = float( line.split()[1] )
+#    except:
+#      time.sleep(0.3)
+#      continue
+#    temp = int(temp * 100)
+#    line = o.stdout.readline()
+#    try:
+#      humi = float( line.split()[1] )
+#    except:
+#      time.sleep(0.3)
+#      continue
+#    humi = int(humi * 100)
+#  break
 
 #----------------------
 # AEH11による照度計測
@@ -49,11 +55,11 @@ else:
   reso = ""
 
 senseillu= 0x0102  # Sensor illuminate of AEH11
-I2CGET = "/usr/sbin/i2cget -y 1 "
-I2CSET = "/usr/sbin/i2cset -f -y 1 "
+I2CGET = "/usr/sbin/i2cget -y 1 "      # 最後の1はBUS IDなので現物に合わせて
+I2CSET = "/usr/sbin/i2cset -f -y 1 "   # 最後の1はBUS IDなので現物に合わせて
 
 AEH11_ADDR = 0x23  # I2Cアドレス
-AEH11_PON = 0x01  # PowerON
+AEH11_PON = 0x01   # PowerON
 AEH11_BCLR = 0x07  # BufferClear
 #AEH11_HIRS = 0x10  # 連続高解像度測定
 AEH11_HIRS = 0x20  # 単発高解像度測定
@@ -137,10 +143,10 @@ illhex = "0x"+str(vstr[4])+str(vstr[5])+str(vstr[2])+str(vstr[3])
 illVal = int(illhex,16)
 
 if reso=="L":
-  illVal = int(illVal*1.79)
+  illVal = int(illVal*1.79)  # 1.79はカバー無しの場合。現物合わせの調整が必要
 else:
-  illVal = int(illVal*0.83)
+  illVal = int(illVal*0.83)  # 0.83はカバー無しの場合。現物合わせの調整が必要
 
-print now, temp, humi, illVal
+print now, temp, humi, illVal    # ここの処理はこれから考える
 
 sys.exit()
